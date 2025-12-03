f97a3c0c2139de34fc6ab029d4e5c87d3feeda91
hyeryeong <gaeryeongeda@naver.com>
2025-12-03 15:11:14 +0900

    기존 파일 업로드

---
+++ vuescript_practice/vue 4일차 예제/src/components/MyComponentItem.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    <h1>MyComponentItem.vue</h1>
+  </div>
+
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+  h1 {
+    color: green;
+  }
+</style>
---
+++ vuescript_practice/vue 5일차 예제/src/components/ParentItem.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <p>{{ myProp.id }}</p>
+  <p>{{ myProp.name }}</p>
+
+</template>
+
+<script setup>
+
+  //  my-prop(케밥케이스) -> myProp(카멜케이스)
+  defineProps({
+    myProp: Object
+  })
+
+</script>
+
+<style scoped>
+
+
+</style>
