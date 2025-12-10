5c809661ba1c6024c7d04fc69267924ba990088c
hyeryeong <gaeryeongeda@naver.com>
2025-12-10 11:13:32 +0900

    기존 파일 업로드

---
+++ vuescript_practice/vue 7일차 예제/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <h1> 오늘의 할일 </h1>
+
+  <h2> 완료된 할일의 개수 : {{ store.doneTodoCount }}</h2>
+
+  <!-- 할일 목록 -->
+  <TodoList/>
+  <!-- 할일 생성 -->
+  <TodoForm/>
+</template>
+
+
+<script setup>
+  // 중앙저장소 (stores의 counter.js) 가져오기
+  import { useCounterStore } from '@/stores/counter';
+
+  import TodoForm from '@/components/TodoForm.vue';
+  import TodoList from '@/components/TodoList.vue';
+
+  const store = useCounterStore()
+
+</script>
+
+
+<style scoped>
+
+
+</style>
---
+++ vuescript_practice/vue 8일차 예제/vue-project/src/views/ArticleView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>보검의 게시판</h1>
+    <RouterLink :to="{ name:'CreateView' }">
+      [CREATE]
+    </RouterLink>
+    <!-- 전체 게시글 목록 -->
+    <ArticleList/>
+  </div>
+</template>
+
+<script setup>
+
+  import ArticleList from '@/components/ArticleList.vue';
+  // 처음 화면에 들어왔을 때 실행
+  import { onMounted } from 'vue';
+  // 중앙저장소
+  import { useArticleStore } from '@/stores/articles';
+
+  import { RouterLink } from 'vue-router';
+
+  const store = useArticleStore()
+
+  // 화면 들어오자마자 전체 게시글 조회하겠다.
+  onMounted(() => {
+    store.getArticles()
+  })
+
+</script>
+
+  
+<style>
+
+
+  
+</style>
